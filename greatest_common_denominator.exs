6542e853065308cbbd6c70d718dc44307eb9a19a
Taras Yermolenko <t.yermolenko@gmail.com>
2024-06-13 23:02:12 +1000

    new: finding GCD while writing some tests upfront

---
+++ greatest_common_denominator.exs
@@ -0,0 +1,28 @@
+defmodule GCD do
+
+ def gcd(a, b) when a < 0, do: gcd(-a, b)
+ def gcd(a, b) when b < 0, do: gcd(a, -b)
+ def gcd(0, b), do: b
+ def gcd(a, 0), do: a
+
+ def gcd(a, b) do
+   case rem(a, b) do
+     0 -> b
+     c -> gcd(b, c)
+   end
+ end
+
+end
+
+
+ExUnit.start()
+defmodule GCDTest do
+  use ExUnit.Case
+
+  test "greatest common denominator" do
+    assert GCD.gcd(36, 63) == 9
+    assert GCD.gcd(-5, 25) == 5
+    assert GCD.gcd(0, 5) == 5
+    assert GCD.gcd(5, 0) == 5
+  end
+end
