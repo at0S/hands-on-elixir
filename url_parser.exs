3b624803e81fc52cfd107c5c0ad66ceea218e221
Taras Yermolenko <t.yermolenko@gmail.com>
2024-07-07 19:57:54 +1000

    new: added case to parse urls with parameters. This whole thing though assumes correctly crafted urls being passed in, so it does not have any error handling for cases the URL which is passed in is invalid

--- url_parser.exs
+++ url_parser.exs
@@ -15,8 +15,30 @@ defmodule UrlParser do
         nil -> "/"
         _   -> "/" <> tail
       end
-      %UrlParser{protocol: protocol, host: host, port: port, path: path}
+
+    [path, parameters] = case String.split(path, "?") do
+      [path, parameters] -> [path, parse_parameters(parameters)]
+      [path] -> [path, %{}]
+    end
+    
+    %UrlParser{protocol: protocol, host: host, port: port, path: path, parameters: parameters}
   end
+
+  def parse_parameters(str) do
+    parameters = String.split(str, "&")
+    parse_keys(parameters, %{})
+  end
+
+  def parse_keys([], acc) do 
+    acc
+  end
+
+  def parse_keys([head | tail], acc) do
+    [key, value] = String.split(head, "=")
+    acc = Map.put(acc, key, value)
+    parse_keys(tail, acc)
+  end
+
 end
 
 ExUnit.start()
@@ -30,7 +52,7 @@ defmodule UrlParserTest do
       host: "www.google.com",
       port: "443",
       path: "/",
-      parameters: nil
+      parameters: %{}
     }
   end
 
@@ -41,7 +63,7 @@ defmodule UrlParserTest do
       host: "www.google.com",
       port: "80",
       path: "/",
-      parameters: nil
+      parameters: %{}
     }
 end
 
@@ -52,27 +74,29 @@ end
       host: "www.google.com",
       port: "8000",
       path: "/",
+      parameters: %{}
     }
   end
 
-#  test "URL with path " do
-#    input_url = "https://www.google.com/search"
-#    assert UrlParser.parse(input_url) == %UrlParser{
-#      protocol: "https",
-#      host: "www.google.com",
-#      port: 443,
-#      path: "/search",
-#    }
-#  end
+  test "URL with path " do
+    dbg(UrlParser.parse("https://www.google.com/search"))
+    assert UrlParser.parse("https://www.google.com/search") == %UrlParser{
+      protocol: "https",
+      host: "www.google.com",
+      port: "443",
+      path: "/search",
+      parameters: %{}
+    }
+  end
 
-#  test "URL with path and parameters" do
-#    input_url = "https://www.google.com/search?q=elixir"
-#    assert UrlParser.parse(input_url) == %UrlParser{
-#      protocol: "https",
-#      host: "www.google.com",
-#      port: 443,
-#      path: "/",
-#      parameters: %{ "q" => "elixir"}
-#    }
-#  end
+  test "URL with path and parameters" do
+    dbg(UrlParser.parse("https://www.google.com/search?q=elixir&lang=en"))
+    assert UrlParser.parse("https://www.google.com/search?q=elixir&lang=en") == %UrlParser{
+      protocol: "https",
+      host: "www.google.com",
+      port: "443",
+      path: "/search",
+      parameters: %{ "q" => "elixir", "lang" => "en"}
+    }
+  end
 end
